59a9d770e070c9f67dbedc51704ef00f99324651
mengqigu_ajk <mengqigu_ajk@58ganji.com>
2018-07-17 10:17:38 +0800

    hello

---
+++ HackerRank/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HackerRank
+//
+//  Created by sullivangu on 7/17/18.
+//  Copyright © 2018 sullivangu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
